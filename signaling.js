82e05fb3a27691772f6dc67e177d05b535277500
Tamer Aktaş <kriptontr@gmail.com>
2018-09-21 23:05:23 +0300

    initial

--- signaling.js
+++ signaling.js
@@ -1,4 +1,51 @@
-const signaling = new SignalingChannel();
-const constraints = {audio: true, video: true};
-const configuration = {iceServers: [{urls: 'stun.l.google.com:19302'}]};
+
+const configuration = {iceServers: [{urls: 'stun:stun.l.google.com:19302'}]};
 const pc = new RTCPeerConnection(configuration);
+console.log(pc)
+pc.onicecandidate = function(icecandidate) {
+
+    // send candidates to other guy
+    var data = {
+        type: 'iceCandidate',
+       // roomId: roomId,
+        payload: icecandidate
+    };
+
+};
+
+discoveryChannel = pc.createDataChannel('discovery', {ordered: true});
+discoveryChannel.onopen = function (a,b) {
+    console.log("opened")
+    console.log(a,b)
+};
+discoveryChannel.onclose = function (a,b) {
+    console.log(a,b)
+};
+const offerOptions = {
+    offerToReceiveAudio: 1,
+    offerToReceiveVideo: 1
+};
+
+discoveryChannel.onerror = function (a,b) {
+
+    console.log(a,b)
+};
+
+pc.createOffer(offerOptions).then(function (offer) {
+    pc.setLocalDescription(offer)
+    document.getElementById("rootSDP").textContent = offer.sdp
+})
+function setRemote() {
+   console.log("setRemote")
+    const answer = {
+        type: 'answer',
+        sdp: document.getElementById("rootSDP").textContent
+    };
+   pc.setRemoteDescription(answer,
+       function (data) {
+           console.log(data)
+   },
+       function (err) {
+           console.log(err)
+       })
+}
